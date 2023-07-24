475159e1779a7b026905d2993bff4daaf15ae814
Mawojast <inet.beck@gmail.com>
2023-07-24 19:28:27 +0200

    Angular - Standalone

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Leards-Standalone',
+  appId: 'net.webbib.leards',
+  appName: ' Leards',
   webDir: 'www',
   server: {
     androidScheme: 'https'
